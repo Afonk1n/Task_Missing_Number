4731039117d8a19a2f4c39af8913c28f5ce7f975
Kirito <afona2001@mail.ru>
2023-11-02 21:29:36 +0500

    solution

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Missing_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {3, 0, 1};
+            //Array.Sort(nums);
+
+            //if (nums[nums.Length - 1] != nums.Length)
+            //{
+            //    Console.WriteLine(nums.Length);
+            //}
+            //else
+            //{
+            //    for (int i = 0; i < nums.Length; i++)
+            //    {
+            //        if (i != nums[i])
+            //        {
+            //            Console.WriteLine(i);
+            //        }
+            //    }
+            //}
+
+            int n = nums.Length;
+            Console.WriteLine(n * (n + 1) / 2 - nums.Sum()); //Arithmetic progression formula
+            Console.ReadLine();
+        }
+    }
+}
